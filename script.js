38b3087d0b604783c06d259fa47adcb8ebca1808
o <osman@marmullaku.ch>
2023-01-15 00:36:37 +0100

    revamp

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('goRightCircle').onclick = function() {
-  document.getElementById('portraits').scrollLeft += 550;
-};
-document.getElementById('goLeftCircle').onclick = function() {
-  document.getElementById('portraits').scrollLeft -= 500;
-};
